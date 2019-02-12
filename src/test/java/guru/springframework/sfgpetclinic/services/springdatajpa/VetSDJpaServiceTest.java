93c37367aee0cf5c3b38de92254fec6a679cb243
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2019-02-12 09:35:11 -0200

    Assignment done

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+	@Mock
+	VetRepository vetRepository;
+	
+	@InjectMocks
+	VetSDJpaService service;
+
+	@Test
+	void testDeleteById() {
+		service.deleteById(1L);
+		
+		verify(vetRepository).deleteById(1L);
+	}
+
+}
